0eb4abb55043186efb15e07ee87b2642cdab6680
Heaven Chen <chenheaven@icloud.com>
2024-12-09 22:20:26 +0900

    [Update-LIFF] Setup eslint and prettier for the project

---
+++ liff/.prettierrc.js
@@ -0,0 +1,12 @@
+const options = {
+  semi: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'lf',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss']
+}
+
+module.exports = options
